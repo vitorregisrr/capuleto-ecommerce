759eb6d1385b8d70b5464fbb910da4ddbff87b35
vitorregisr <vitorregisrr@gmail.com>
2019-12-18 13:50:41 -0300

    Add produtos page

--- js/misc.js
+++ js/misc.js
@@ -5,11 +5,9 @@
     new WOW().init();
     $('.lazy').lazyload();
 
-
     const mainNav = $('#main-nav');
-    
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 300) {
+        if ($(window).scrollTop() > 80) {
             mainNav.addClass('sticky');
         } else {
             mainNav.removeClass('sticky');
@@ -17,21 +15,48 @@
     });
 
     // Validação newsletter
-    // $("#newsletter-form").validate({
-    //     rules: {
-    //         email: {
-    //             required: true,
-    //             email: true
-    //         },
-    //     },
-    
-    //     messages: {
-    //         email: {
-    //             required: 'Precisamos saber seu email',
-    //             email: 'Email inválido'
-    //         }
-    //     }
-    // });
+    $("#newsletter-form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+        },
+
+        messages: {
+            email: {
+                required: 'Por favor, insira um e-mail válido!',
+                email: 'Email inválido'
+            }
+        }
+    });
+
+    // Dropdown da página de produtos
+    $('#filtros-toggler').click(function () {
+        $('#filtros-backdrop').addClass('active');
+        $('#filtros-dropdown').addClass('active');
+    });
+
+    $('#filtros-dropdown .close').click(function () {
+        $('#filtros-backdrop').removeClass('active');
+        $('#filtros-dropdown').removeClass('active');
+    });
+
+    $('#filtros-backdrop').click(function () {
+        $('#filtros-backdrop').removeClass('active');
+        $('#filtros-dropdown').removeClass('active');
+    });
+
+    // Select customizado da página de produtos (filtro de ordernação)
+    const customSelects = $('.custom_select--radio');
+    customSelects.picker({
+        containerClass: 'custom-select--radio'
+    });
+
+    // Função que submete o formulário ao selecionar a ordem
+    $('.pc-list li').click( function(){
+        $('#ordernar-form').submit();
+    });
 
     window.dispatchEvent(new Event('resize'));
 
